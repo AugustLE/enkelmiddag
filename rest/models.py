e5c458a794aabbeed40bbcc174eea2ad1d7a1f8a
August Lund Eilertsen <le_lund@hotmail.com>
2017-12-02 22:51:48 +0100

    minfix

--- rest/models.py
+++ rest/models.py
@@ -52,7 +52,7 @@ def ingredient_type_dirctory_path(instance, filename):
 
 class IngredientType(models.Model):
 
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     plural_name = models.CharField(max_length=100, null=True, blank=True)
     singular_name = models.CharField(max_length=100, null=True, blank=True)
     image = models.ImageField(upload_to=ingredient_type_dirctory_path, null=True, blank=True)
